5344bf61cf580eb4f625a96cde3bc6fe25874d02
UrbanIII <maximilian.marienhagen@gmail.com>
2018-05-27 22:57:12 +0200

    finishes fixing this (hopefully)

--- Kapillarität und Viskosität/auswertung3_new.py
+++ Kapillarität und Viskosität/auswertung3_new.py
@@ -5,42 +5,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as opt
 
+golden_ratio = (1+np.sqrt(5))/2
+width = 6.202 #Measured in Latex! (inches)
+height = width/golden_ratio #This is supposed to look really nice.
+
 settings = {
-    'figure.figsize': [6,4], #Angabe in Zoll
+    'figure.figsize': [width,height],
     'font.family': 'serif',
-    'backend': 'pdf',
-    'font.size': 12,
     'figure.autolayout': True,
-    'axes.titlesize': 'medium',
-    'legend.fontsize': 'medium'
+    'figure.titlesize': 14,
+    'axes.titlesize': 12,
+    'legend.fontsize': 10,
+    'xtick.labelsize': 10,
+    'ytick.labelsize': 10
 }
 
 plt.rcParams.update(settings)
-
+#print plt.rcParams
 data = np.loadtxt("data2b")
 t = data[:,0]
 h = data[:,1]
+terr = 0.005*t+0.3 #0.3 = Schätzwert, Rest siehe Praktikumsbuch
+herr = 0.2 #Schätzwert in cm
+lnerr = np.sqrt((herr/h)**2+(herr/h[0])**2) #Gauß
 
-plt.yscale('log')
 plt.title('Ausfluss des Wassers durch die dünnste Kapillare')
-#plt.errorbar(t,h,xerr=0.3,yerr=0.2,fmt="go",label="Messwerte",ecolor="red")
-plt.plot(t,h/h[0],'gx',label='Messwerte')
+plt.errorbar(t,np.log(h/h[0]),xerr = terr,yerr=lnerr,fmt="go",label="Messwerte",ecolor="red")
 plt.xlabel(r'$t$ [s]')
-plt.ylabel(r'$\frac{h(t)}{h_0}$')
+plt.ylabel(r'$\ln\left(\frac{h(t)}{h_0}\right)$')
 
-def f(x,a):
-    return np.exp(a*x)
+def f(x,a,b): #Fitfunktion
+    return a*x+b
 
-estimate = np.pi*1000*9.81*(0.0004)**4/(8*8.9*10**-4*0.23*(10**-6/0.05))
-
-params, pcov = opt.curve_fit(f, t, h/h[0], p0 = estimate) #log = ln
+params, pcov = opt.curve_fit(f, t, np.log(h/h[0])) #log = ln
 perr = np.sqrt(np.diag(pcov))
-print 'Anstieg:\t', params[0], '+/- ',perr[0],
+print 'Anstieg:\t', params[0], '+/- ',perr[0]
+print 'Achsenabschnitt:\t', params[1], '+/-', perr[1]
 
 x_axis = np.linspace(0,t[-1],100)
-plt.plot(np.linspace(0,t[-1],100), f(np.linspace(0,t[-1],100),params[0]),label='Fit')
-
-plt.legend(loc='lower right')
+plt.plot(x_axis, f(x_axis,params[0],params[1]),label=r'(-1.356 $\pm$ 0.004)$\cdot 10^{-3}\,\mathrm{s}^{-1}\cdot t\,-(5\pm8)\cdot 10^{-4}$')
+plt.legend(loc='upper right')
 
 fig = plt.gcf()
 plt.show()
